52ac680a30576b7b3bc5f61aaa429e6e3ab50019
Konstans8 <kostian73kostian@gmail.com>
2024-11-27 00:12:53 +0100

    'add_vue'

--- main.py
+++ main.py
@@ -8,11 +8,25 @@ from fastapi.params import Depends
 # from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
+from fastapi.middleware.cors import CORSMiddleware
 from models import Base, User, Post
 from database import engine, session_local
 from schemas import UserCreate, User as DbUser, PostCreate, PostResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 # @app.get("/")
 # async def home() -> str:
@@ -78,9 +92,17 @@ async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostRe
     return db_post
 
 
-@app.get("/post/", response_model=List[PostResponse])
+@app.get("/posts/", response_model=List[PostResponse])
 async def posts(db: Session = Depends(get_db)):
     return db.query(Post).all()
+
+
+@app.get("/users/{name}", response_model=DbUser)
+async def posts(name: str, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.name == name).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found!')
+    return db_user
 
 
 # users = [
